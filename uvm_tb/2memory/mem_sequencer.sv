5b2fbefe7e53bc42df53ccfa355fde2613b7c603
Carl Yan <94885426+Carl7yan@users.noreply.github.com>
2023-11-03 23:05:31 +0800

    Delete uvm_tb/2memory/mem_sequencer.sv

--- uvm_tb/2memory/mem_sequencer.sv
+++
@@ -1,9 +0,0 @@
-class mem_sequencer extends uvm_sequencer#(mem_seq_item);
-
-  `uvm_component_utils(mem_sequencer) 
-
-  function new(string name, uvm_component parent);
-    super.new(name,parent);
-  endfunction
-  
-endclass
